6ee95e7166bf8e807f33b577039943665e9535c6
Alexis Mettola <alexismettola@icloud.com>
2024-12-01 13:51:30 -0300

    add:navbar, heroSection, productosSection ok

---
+++ src/data/productos.js
@@ -0,0 +1,58 @@
+export const productos = [
+    {
+      id: 1,
+      tipo: "Utensillos",
+    },
+    {
+      id: 2,
+      tipo: "Electro",
+    },
+    {
+      id: 3,
+      tipo: "Electro",
+    },
+    {
+      id: 4,
+      tipo: "Herramientas",
+    },
+    {
+      id: 5,
+      tipo: "Electro",
+    },
+    {
+      id: 6,
+      tipo: "Electro",
+    },
+    {
+      id: 7,
+      tipo: "Electro",
+    },
+    {
+      id: 8,
+      tipo: "Electro",
+    },
+    {
+      id: 9,
+      tipo: "Electro",
+    },
+    {
+      id: 10,
+      tipo: "Electro",
+    },
+    {
+      id: 11,
+      tipo: "Electro",
+    },
+    {
+      id: 12,
+      tipo: "Electro",
+    },
+    {
+      id: 13,
+      tipo: "Electro",
+    },
+    {
+      id: 14,
+      tipo: "Electro",
+    },
+  ];
